c10eb2b8b26a27b4b198e184a188e85a65fcc785
Daniel Banach <djbanach@comcast.net>
2019-06-25 18:13:08 -0700

    added appbar and table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppBarTop from './Components/AppBarTop';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     width: '100%',
+//     marginTop: theme.spacing(3),
+//     overflowX: 'auto',
+//   },
+//   table: {
+//     minWidth: 650,
+//   },
+// }));
 
 class App extends Component {
 
@@ -19,13 +37,35 @@ class App extends Component {
     .catch(err => console.error(err))
   }
 
-  renderProduct = ({ product_id, name, amount, price }) => <div key={product_id}>{name} {amount} {price}</div>
 
   render() {
     const { products } = this.state;
+
     return (
       <div className="App">
-        {products.map(this.renderProduct)}
+        <AppBarTop />
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Amount</TableCell>
+                <TableCell align="right">Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map(product => (
+                <TableRow key={product.product_id}>
+                  <TableCell component="th" scope="row">
+                    {product.name}
+                  </TableCell>
+                  <TableCell align="right">{product.amount}</TableCell>
+                  <TableCell align="right">{product.price}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     );
   }
